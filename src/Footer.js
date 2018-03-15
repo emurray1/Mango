844ecf399395109d7c98f4d74095b68932686a93
Eric Murray <eric.murray@dealer.com>
2018-03-15 18:58:00 -0400

    updating sass

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
 	render() {
 		return (
     		<div className="footer container-fluid">
-    			<div className="container">
+    			<div className="container text-white">
     				<p>Footer</p>
     			</div>
     		</div>
--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -46,7 +46,7 @@ const HomePage = () => {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="container">
+                    <div className="container text-white">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla mattis lectus sed finibus. Ut non varius mauris. Curabitur vulputate sit amet mi egestas sollicitudin. Sed mattis augue quis malesuada vehicula. Aliquam eget orci velit. Duis varius commodo odio. Nullam id iaculis justo. Donec aliquam cursus leo, non volutpat nisl finibus eget. Proin vel orci sit amet massa posuere sodales. Quisque luctus diam eget sollicitudin hendrerit. Pellentesque augue nibh, congue eu neque eu, maximus consequat tellus. Sed auctor quam et ante blandit, sed condimentum eros pellentesque. Nam et porttitor ligula, non porttitor urna. Integer ut tristique dolor, sagittis sollicitudin purus. In hac habitasse platea dictumst.</p>
                     </div>
                 </div>
