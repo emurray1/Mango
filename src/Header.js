50989ffb22229cfe2b930ede142a0008e772d14e
emurray1 <murray.m.eric@gmail.com>
2018-03-18 20:51:31 -0400

    updated Header

--- src/Header.js
+++ src/Header.js
@@ -5,31 +5,28 @@ import MangoLogo from './images/Mango_Header.png';
 import './header.css';
 
 const Header = () => (
-	<div className="header container-fluid bg-primary">
-   		<div className="container">
-   			<nav className="navbar navbar-expand-lg">
-   				<Link to="/">
-				    <img className="img" alt='Mango Development Logo'  src={MangoLogo} />
-			    </Link>
-			    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>		
-				<div className="collapse navbar-collapse">
-					<ul className="navbar-nav mr-auto">
-						 <li className="nav-item">
-						 	<NavLink exact className="nav-link text-secondary" to="/product">What We Do</NavLink>
-						 </li>
-						  <li className="nav-item">
-						 	<NavLink exact className="nav-link text-secondary" to="/portfolio">What We Did</NavLink>
-						 </li>
-						  <li className="nav-item">
-						 	<NavLink exact className="nav-link text-secondary" to="/contact">Contact</NavLink>
-						 </li>
-			    	</ul>
-		  		</div>
-		  	</nav>
-	    </div>
-	</div>
+   	<nav className="navbar bg-primary navbar-expand-lg sticky-top container-fluid">
+   		<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span className="navbar-toggler-icon"></span>
+		</button>	
+   		<Link to="/">
+		    <img className="img" alt='Mango Development Logo'  src={MangoLogo} />
+		</Link>
+		<div className="collapse navbar-collapse">
+			<ul className="navbar-nav ml-auto">
+				<li className="nav-item">
+					<NavLink exact className="nav-link text-secondary" to="/product">What We Do</NavLink>
+				</li>
+				<li className="nav-item">
+					<NavLink exact className="nav-link text-secondary" to="/portfolio">What We Did</NavLink>
+				</li>
+				<li className="nav-item">
+				 	<NavLink exact className="nav-link text-secondary" to="/contact">Contact</NavLink>
+				</li>
+		   	</ul>
+	  	</div>
+	</nav>
+
 )
  
 export default Header;
